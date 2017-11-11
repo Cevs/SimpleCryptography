7e58b383f3563d4e8c5fa51629ce892782654f8c
Alen Martničević <martincevic.alen@gmail.com>
2017-11-11 21:43:39 +0100

    Initial commit

---
+++ src/main/java/com/simplecryptography/SimpleCryptographyApplication.java
@@ -0,0 +1,12 @@
+package com.simplecryptography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCryptographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCryptographyApplication.class, args);
+	}
+}
